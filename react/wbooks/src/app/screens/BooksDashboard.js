5667282881822a5fc9874ab8469747d07530bee9
carovaldezg <caro.valdezg@gmail.com>
2017-10-02 14:59:12 -0300

    feature complete, ready for code review

--- react/wbooks/src/app/screens/BooksDashboard.js
+++ react/wbooks/src/app/screens/BooksDashboard.js
@@ -5,34 +5,84 @@ import Book from '../../app/components/Book.js';
 import search_icon from '../ASSETS/search.svg';
 
 class BooksDashboard extends React.Component {
+
+  state = {
+    filterType: '',
+    filterText: '',
+    activeFilterType: '',
+    activeFilterText: ''
+  }
+
+  handleTypeChange = (event) => {
+    this.setState({
+      filterType: event.target.value
+    });
+  }
+
+  handleTextChange = (event) => {
+    this.setState({
+      filterText: event.target.value
+    });
+  }
+
+  filteredBooks = () => {
+    return booksList.filter(book => {
+      if (this.state.activeFilterType === 'author') {
+        return book.author.includes(this.state.activeFilterText);
+      } else {
+        return book.title.includes(this.state.activeFilterText);
+      }
+    });
+  }
+
+  handleFilter = () => {
+    this.state.filterText
+    return this.setState(prevState => ({
+      activeFilterText: prevState.filterText,
+      activeFilterType: prevState.filterType,
+    }));
+  }
+
   render() {
-    const booksItems = booksList.map(
+    const booksItems = this.filteredBooks().map(
       (bookItem) => (
         <Book
           book={bookItem}
-          key = {bookItem.id}
+          key={bookItem.id}
         />
       )
     );
+
     return (
       <div>
         <div className="book-list">
           <form className="search-form">
-            <select className="search-input selector">
+            <select className="search-input selector"
+            onChange={this.handleTypeChange} value={this.state.filterType}>
               <option value="" selected disabled>Seleccionar filtro</option>
               <option value="title">Titulo</option>
               <option value="author">Autor</option>
             </select>
-            <input type="text" name="name" placeholder="Buscar..." className="search-input"/>
-            <img src = {search_icon} className="search-icon" />
+            <input
+            type="text" name="name" placeholder="Buscar..."
+              className="search-input" onChange={this.handleTextChange}
+              value={this.state.filterText}/>
+            <img src = {search_icon} className="search-icon"
+              onClick={this.handleFilter} />
           </form>
           <div className="books-container">
-            {booksItems}
+            {
+              booksItems.length === 0 ? (
+                <h2>La busqueda no encontró resultados.</h2>
+              ) : (
+                booksItems
+              )
+            }
           </div>
         </div>
       </div>
     );
-  };
+  }
 }
 
 export default BooksDashboard;
